2a6d92e2699c8e35d410e09a7cf4afc75602c42f
nhuanh <nhuanhnguyen112715@gmail.com>
2026-04-13 13:59:23 +0700

    Cập nhật nội dung file AuctionManager.java

--- src/manager/AuctionManager.java
+++ src/manager/AuctionManager.java
@@ -1,4 +1,37 @@
 package manager;
 
+import model.Auction;
+import model.User;
+import model.BidTransaction;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuctionManager {
+    private static AuctionManager instance;
+
+    private Map<Long, Auction> activeAuction;
+
+    private User currentUser;
+
+    private AuctionManager() {}
+
+    public static AuctionManager getInstance() {
+        return null;
+    }
+
+    public void setCurrentUser(User user) {}
+
+    public User getCurrentUser() {
+        return null;
+    }
+
+    public Auction createAuction(Auction newAuction) {
+        return newAuction;
+    }
+
+    public Auction getAuctionById(Long id) {
+        return null;
+    }
+
+    public void placeBid(User user, Long auctionId, double amount) {}
 }
