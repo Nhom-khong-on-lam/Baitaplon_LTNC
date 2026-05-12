d382bbc419d7e632ad3fb9c9f5bffcd3d6125d8b
25023260-dot <25023260@vnu.edu.vn>
2026-05-12 19:46:10 +0700

    Chuan bi cap nhat tu develop sang bid

--- Server/src/main/java/server/common/model/PaymentDTO.java
+++ Server/src/main/java/server/common/model/PaymentDTO.java
@@ -1,4 +1,71 @@
 package server.common.model;
 
+import java.time.LocalDateTime;
+
 public class PaymentDTO {
+    private Long id;
+    private Long auctionId;
+    private Long userId;
+    private double amount;
+    private String paymentMethod;
+    private String status;
+    private LocalDateTime paymentDate;
+
+    // Constructor không tham số cho DAO
+    public PaymentDTO() {
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(Long auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public LocalDateTime getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDateTime paymentDate) {
+        this.paymentDate = paymentDate;
+    }
 }
